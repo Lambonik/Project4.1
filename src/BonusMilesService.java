c8bdc26af674a059fff086357bfcbb1d633f56d7
Lambonik <nikita1994g@mail.ru>
2023-02-26 17:32:06 +0300

    first commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,10 @@
+public class BonusMilesService {
+    public int calculate(int price) {
+        int value = 20;
+        int bonus;
+        bonus = price / value;
+
+        return bonus;
+
+    }
+}
